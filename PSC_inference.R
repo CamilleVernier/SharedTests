2e985bbfa07d0574556e42f61c50c4e0bbeae478
CamilleVernier <verniercamille@hotmail.fr>
2018-04-04 13:12:05 +0200

    add first original files

---
+++ PSC_inference.R
@@ -0,0 +1,89 @@
+### Inference with the summary likelihood method 
+### under a population size change (PSC) model.
+###
+### Alexandre Gouy - alexandre.gouy@iee.unibe.ch
+###
+### This script allows to perform simulations using the software 
+### IBDsim and to infer the model parameters using summary likelihood
+
+# Required: this R script, "PSC_simulation.R", and
+# the IBDsim sources which can be found here: 
+# http://www1.montpellier.inra.fr/CBGP/software/ibdsim/download.html
+# (Leblois et al. 2009 Mol. Ecol. Resources)
+
+# Important note:
+# This script is designed to perform one simulation at each call of IBDsim,
+# which is not optimal in terms of computation times. In our study, 
+# to save computation time, we generated the whole distribution of 
+# summary statistics with one call of IBDsim (i.e. 1000 simulations
+# for one set of parameters). But this procedure involves manual 
+# steps, especially during the iteration procedure, to project and 
+# add the new summary statistics.
+# Therefore, we preferred to provide a script which is functional 
+# for any user, even if it is substantially slower.
+
+library(Infusion)
+library(caret)
+
+setwd(dir = "/home/vernierc/Documents/Logiciels/Nimbus2000/")
+
+# We load a function to run PSC simulations and return the 6 summary 
+# statistics used in this study:
+source("PSC_simulation.R") 
+#IBDSimExec<-"./ibdsimV2.0-win-i386.exe" #IBDsim executable if ran on Windows
+
+# If the user wants to run this script on a Linux platform, the IBDsim sources
+# must be compiled thanks to the script "compile.sh" provided with the sources
+# on IBDsim website.
+# The g++ compiler must be installed on the user's system.
+IBDSimExec<-"./IBDSim" # if ran on Linux
+
+
+# We first define the parameter grid.
+# The number of sets is equal to the number of empirical distribution 
+# desired for the first iteration (here, 100).
+parsp <- init_grid(lower=c(log10theta=-1,log10a=-3,log10tau=-4),
+                   upper=c(log10theta=1.5,log10a=3,log10tau=1),
+                   nUnique=100)
+
+# We compute a pseudo-observed set of summary statistics with
+# with arbitrary parameters values, e.g. theta = 1, a = 1, tau = -1
+sobs <- IBDSim_wrapper(log10theta=1,log10a =-1,log10tau=-2)
+
+# We generate the first set of empirical distributions
+# Note: To avoid these computationnaly intensive first steps, 
+# an initial set of projected summary statistics has been provided
+# (you can skip the next lines and load the simulations l.68)
+simuls <- add_simulation(NULL,Simulate="IBDSim_wrapper",par.grid=parsp)# nRealizations = 1
+
+# We project the summary statistics with neural networks.
+# Statistics to be projected:
+allstats <- c("H","VarK","K","M","varH","f") 
+
+thetaproj <- project("log10theta",stats=allstats,data=simuls,method="neuralNet")
+aproj <- project("log10a",stats=allstats,data=simuls,method="neuralNet")
+tauproj <- project("log10tau",stats=allstats,data=simuls,method="neuralNet")
+
+# We apply projections on simulated statistics:
+corrSimuls <- project(simuls,projectors=list("THETA"=thetaproj,"A"=aproj,"TAU"=tauproj))
+corrSobs <- project(sobs,projectors=list("THETA"=thetaproj,"A"=aproj,"TAU"=tauproj))
+
+# load("PSC_simulations.rda") # loads the projected summary statistics
+# We then infer the summary-likelihood surface:
+densv <- infer_logLs(corrSimuls,stat.obs=corrSobs)
+slik <- infer_surface(densv)
+slik <- MSL(slik)
+
+# We iterate to improve parameter estimation:
+nIterations <- 3
+for(i in 1:nIterations) {
+  slik <- refine(slik)
+}
+
+# The slik object contains the ML estimates and 95% CIs:
+summary(slik)
+
+# We can also get the x% CIs (here, 90% CIs):
+confint(slik,"log10theta",level=0.9)
+confint(slik,"log10a",level=0.9)
+confint(slik,"log10tau",level=0.9)
---
+++ PSC_simulation.R
@@ -0,0 +1,67 @@
+### This script generates the six statistics used in this study from 
+# a PSC model with a given set of parameter, by calling IBDsim from R.
+# IBDsim v2.0 reference: Leblois et al. 2009 MolEcol Ressources
+
+IBDSim_wrapper<-function(log10theta,
+                 log10a,
+                 log10tau,
+                 nsim=1,
+                 nloc=20, # number of loci
+                 sampleSize=10, # number of individuals to simulate
+                 mu=5e-2, # mutation rate
+                 execName="./IBDSim"){ # executable name
+  #conversion from the scaled parameters:
+  a <- 10^log10a
+  N0<-(10^log10theta)/mu
+  t<-(10^log10tau)*N0
+  
+  # we write the input file for IBDsim:
+  write.table(paste("%%%%% SIMULATION PARAMETERS %%%%%%%%%%%%
+                    Run_Number=",nsim,"
+                    Migraine_Settings=F
+                    Ploidy=Diploid
+                    %%%%% MARKERS PARAMETER S%%%%%%%%%%%%%%%
+                    Locus_Number=",nloc,"
+                    Mutation_Rate=",format(mu, scientific = FALSE),"
+                    Mutation_Model=GSM
+                    Allelic_Upper_Bound=200
+                    %%%%%%%% DEMOGRAPHIC OPTIONS %%%%%%%%%%%%%
+                    %% LATTICE
+                    Lattice_SizeX=1
+                    Lattice_SizeY=1
+                    Ind_Per_Pop=",N0,"
+                    %% SAMPLE
+                    Sample_SizeX=1
+                    Sample_SizeY=1
+                    Min_Sample_CoordinateX=1
+                    Min_Sample_CoordinateY=1
+                    Ind_Per_Pop_Sampled=",sampleSize,"
+                    %% DISPERSAL
+                    Dispersal_Distribution=b
+                    TotalEmigrationRate=0
+                    Continuous_Deme_Size_Variation=Exponential
+
+                    NewDemographicPhaseAt=",t,"
+                    Continuous_Deme_Size_Variation=None
+                    Ind_Per_Pop=",format(N0*a, scientific = FALSE),"
+                    Genepop=false
+                    DiagnosticTables=[Hexp,Fis,Allelic_Variance,Iterative_Statistics]",sep=""),
+              file="IbdSettings.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
+  
+  
+  # We call IBDsim through the system command, 
+  # it will read the "IbdSettings.txt" file and run the simulations:
+  system(execName)
+  
+  # We then load the simulated summary statistics and only keep the 6 used in this study:
+  sumstats<-read.table("Iterative_Statistics_postdisp_PerLocus.txt",sep="",skip=1)
+  dat<-sumstats[,(dim(sumstats)[2]-3):dim(sumstats)[2]] # H, K, M and varK
+  colnames(dat)<-c("H","VarK","K","M")
+  dat$varH<-apply(sumstats[,1:20],1,var) #variance of H
+  hobs<-read.table("Various_Statistics_postdisp.txt",sep="",skip=9,nrows=1)[,5]
+  hexp<-read.table("Various_Statistics_postdisp.txt",sep="",skip=10,nrows=1)[,10] 
+  dat$f<-1-(hobs/hexp) # F
+
+  dat<-unlist(dat) # required to be read by Infusion
+  return(dat) # returns the 6 summary statistics
+}
