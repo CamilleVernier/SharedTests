3155084f9c234ab1d5c13b82e586559fc3f8e493
CamilleVernier <verniercamille@hotmail.fr>
2018-04-24 14:32:54 +0200

    tempfile

--- PSC_inference.R
+++ PSC_inference.R
@@ -22,11 +22,14 @@
 # Therefore, we preferred to provide a script which is functional 
 # for any user, even if it is substantially slower.
 
+rm(list = ls())
+options(error=recover) 
+
 library(Infusion)
 library(caret)
 
 setwd(dir = "/home/vernierc/Documents/GitCamille/SharedTests/")
-setwd("/Users/raph/Documents/Taf/EtudiantsPostdocVisiteurs+Stages+Theses/_CamilleVernier/GitHub/SharedTests/") #pour Raph
+#setwd("/Users/raph/Documents/Taf/EtudiantsPostdocVisiteurs+Stages+Theses/_CamilleVernier/GitHub/SharedTests/") #pour Raph
 
 # We load a function to run PSC simulations and return the 6 summary 
 # statistics used in this study:
@@ -37,33 +40,73 @@ source("PSC_simulation.R")
 # must be compiled thanks to the script "compile.sh" provided with the sources
 # on IBDsim website.
 # The g++ compiler must be installed on the user's system.
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
+
+
 if(get_os()=="linux") {
-  IBDSimExec<-"./IBDSim"
+  IBDSimExec<-"../IBDSim"
 }  else if(get_os()=="osx") {
-    IBDSimExec<-"./IBDSimMac"
-    } else {IBDSimExec<-"./ibdsimv2.0_Win7.exe"}
+    IBDSimExec<-"../IBDSimMac"
+    } else {IBDSimExec<-"../ibdsimv2.0_Win7.exe"}
 
 
 # We first define the parameter grid.
 # The number of sets is equal to the number of empirical distribution 
 # desired for the first iteration (here, 100).
-parsp <- init_grid(lower=c(log10theta=-1,log10a=-3,log10tau=-4),
-                   upper=c(log10theta=1.5,log10a=3,log10tau=1),
+parsp <- init_grid(lower=c(log10theta=-1,log10a=-2,log10tau=-1.5),
+                   upper=c(log10theta=2,log10a=1,log10tau=1.5),
                    nUnique=50)
 
+
+#10^parsp[,1]/mu
+#10^parsp[,2]*10^parsp[,1]/mu
+#10^parsp[,3]*10^parsp[,1]/mu
+mu <- 0.05
+
+message("SIM: Nact(min/max)=",paste(min(10^parsp[,1]/mu),max(10^parsp[,1]/mu)," "),
+        "T(min/max)=",paste(min(10^parsp[,3]*10^parsp[,1]/mu),max(10^parsp[,3]*10^parsp[,1]/mu)," "),
+        "Nanc(min/max)=",paste(min(10^parsp[,2]*10^parsp[,1]/mu),max(10^parsp[,2]*10^parsp[,1]/mu)," "))
+
+
+
 # We compute a pseudo-observed set of summary statistics with
-# with arbitrary parameters values, e.g. theta = 1, a = 1, tau = -1
-sobs <- IBDSim_wrapper(log10theta=1,log10a =-1,log10tau=-2)
+# with arbitrary parameters values, e.g. theta = 3, a = 3, tau = -1
+log10thetaOBS <- 1.5
+log10aOBS <- -1.5
+log10tauOBS <- -0.2
+
+
+sobs <- IBDSim_wrapper(log10theta=log10thetaOBS,log10a =log10aOBS,log10tau=log10tauOBS,execName=IBDSimExec)
+
+message("OBS: Nact=",10^log10thetaOBS/mu,
+        "; T=",10^log10tauOBS*10^log10thetaOBS/mu,
+        "; Nanc=",10^log10aOBS*10^log10thetaOBS/mu)
+
 
 # We generate the first set of empirical distributions
-# Note: To avoid these computationnaly intensive first steps, 
+# Note: To avoid these computationnaly intensive first steps,
 # an initial set of projected summary statistics has been provided
 # (you can skip the next lines and load the simulations l.68)
-simuls <- add_simulation(NULL,Simulate="IBDSim_wrapper",par.grid=parsp,nRealizations=20)# nRealizations = 1
+simuls <- add_simulation(NULL,Simulate="IBDSim_wrapper",par.grid=parsp, nRealizations = 100, nb_cores = 6)# nRealizations = 1
+
 
 # We project the summary statistics with neural networks.
 # Statistics to be projected:
-allstats <- c("H","VarK","K","M","varH","f") 
+allstats <- c("H","VarK","K","M","varH","f")
 
 thetaproj <- project("log10theta",stats=allstats,data=simuls,method="neuralNet")
 aproj <- project("log10a",stats=allstats,data=simuls,method="neuralNet")
@@ -73,17 +116,19 @@ tauproj <- project("log10tau",stats=allstats,data=simuls,method="neuralNet")
 corrSimuls <- project(simuls,projectors=list("THETA"=thetaproj,"A"=aproj,"TAU"=tauproj))
 corrSobs <- project(sobs,projectors=list("THETA"=thetaproj,"A"=aproj,"TAU"=tauproj))
 
-load("PSC_simulations.rda") # loads the projected summary statistics
+#load("PSC_simulations.rda") # loads the projected summary statistics
 # We then infer the summary-likelihood surface:
 densv <- infer_logLs(corrSimuls,stat.obs=corrSobs)
 slik <- infer_surface(densv)
 slik <- MSL(slik)
 
 # We iterate to improve parameter estimation:
-nIterations <- 3
+nIterations <- 1
 for(i in 1:nIterations) {
   slik <- refine(slik)
 }
+#Error in .calc_all_slices(object, fittedPars, color.palette) : 
+# objet 'plot.axes' introuvable
 
 # The slik object contains the ML estimates and 95% CIs:
 summary(slik)
--- PSC_simulation.R
+++ PSC_simulation.R
@@ -6,17 +6,34 @@ IBDSim_wrapper<-function(log10theta,
                  log10a,
                  log10tau,
                  nsim=1,
-                 nloc=20, # number of loci
-                 sampleSize=10, # number of individuals to simulate
+                 nloc=40, # number of loci
+                 sampleSize=100, # number of individuals to simulate
                  mu=5e-2, # mutation rate
-                 execName="./IBDSim"){ # executable name
+                 execName="../IBDSim"){ # executable name
   #conversion from the scaled parameters:
+  # créer dossier simul + chiffre aleatoire
+  # setwd dans le dossier
+ 
+  # name <- paste("sim",sample(1:10000, 1), sep="_")
+  curDir<-getwd()
+  dir1 <- tempfile(pattern = "sim", tmpdir =curDir )
+  dir.create(dir1)
+  #message("Directory created:",dir1)
+  setwd(dir = dir1)
+  #file.copy("../IBDSim", dir1)
+  file.copy("/home/vernierc/Documents/GitCamille/SharedTests/IbdSettings.txt", dir1)
+  #file.copy("/Users/raph/Documents/Taf/EtudiantsPostdocVisiteurs+Stages+Theses/_CamilleVernier/GitHub/SharedTests/IbdSettings.txt", dir1) #pour Raph
+  
+  
+  #message("Files in the currect directory: ", paste(list.files(path = ".", all.files = TRUE,recursive = TRUE, include.dirs = TRUE, no.. = TRUE)," "))
+  
+
   a <- 10^log10a
   N0<-(10^log10theta)/mu
   t<-(10^log10tau)*N0
-  
+  if (t<1) t=1
   Seed <- 1234567 + sample(1:10000, 1)
-  
+
   # we write the input file for IBDsim:
   write.table(paste("%%%%% SIMULATION PARAMETERS %%%%%%%%%%%%
                     Run_Number=",nsim,"
@@ -51,37 +68,43 @@ IBDSim_wrapper<-function(log10theta,
                     Genepop=false
                     DiagnosticTables=[Hexp,Fis,Allelic_Variance,Iterative_Statistics]",sep=""),
               file="IbdSettings.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
-  
-  
-  # We call IBDsim through the system command, 
+
+
+  # We call IBDsim through the system command,
   # it will read the "IbdSettings.txt" file and run the simulations:
-  system(execName,ignore.stdout=TRUE,ignore.stderr=TRUE)
-  
+  system(execName, ignore.stdout = TRUE)
+
   # We then load the simulated summary statistics and only keep the 6 used in this study:
   sumstats<-read.table("Iterative_Statistics_postdisp_PerLocus.txt",sep="",skip=1)
-  dat<-sumstats[,(dim(sumstats)[2]-3):dim(sumstats)[2]] # H, K, M and varK
-  colnames(dat)<-c("H","VarK","K","M")
-  dat$varH<-apply(sumstats[,1:20],1,var) #variance of H
+  
+  dat<-sumstats[,(dim(sumstats)[2]-4):dim(sumstats)[2]][c(1,3,5,2)] # H, K, M and varK
+  colnames(dat)<-c("H","M","K","VarK") # H = Hexp ?
+  dat$varH<-apply(sumstats[,21:40],1,var) # variance of H ##modifié Hexp?
   hobs<-read.table("Various_Statistics_postdisp.txt",sep="",skip=9,nrows=1)[,5]
-  hexp<-read.table("Various_Statistics_postdisp.txt",sep="",skip=10,nrows=1)[,10] 
-  dat$f<-1-(hobs/hexp) # F
+  hexp<-read.table("Various_Statistics_postdisp.txt",sep="",skip=10,nrows=1)[,10]
+  dat$f<-1-(hobs/hexp) # F  # différent de 0
 
+  setwd("../")
+  unlink(dir1, recursive =TRUE) 
+  
+  
   dat<-unlist(dat) # required to be read by Infusion
   return(dat) # returns the 6 summary statistics
-}
 
-get_os <- function(){
-  sysinf <- Sys.info()
-  if (!is.null(sysinf)){
-    os <- sysinf['sysname']
-    if (os == 'Darwin')
-      os <- "osx"
-  } else { ## mystery machine
-    os <- .Platform$OS.type
-    if (grepl("^darwin", R.version$os))
-      os <- "osx"
-    if (grepl("linux-gnu", R.version$os))
-      os <- "linux"
-  }
-  tolower(os)
 }
+# 
+# get_os <- function(){
+#   sysinf <- Sys.info()
+#   if (!is.null(sysinf)){
+#     os <- sysinf['sysname']
+#     if (os == 'Darwin')
+#       os <- "osx"
+#   } else { ## mystery machine
+#     os <- .Platform$OS.type
+#     if (grepl("^darwin", R.version$os))
+#       os <- "osx"
+#     if (grepl("linux-gnu", R.version$os))
+#       os <- "linux"
+#   }
+#   tolower(os)
+# }
