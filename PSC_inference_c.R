dcbbd04bf329bcbabd3b1663c635182df26dc011
CamilleVernier <verniercamille@hotmail.fr>
2018-05-23 20:48:55 +0200

    Modifs enregistrement données

--- PSC_inference_c.R
+++ PSC_inference_c.R
@@ -36,6 +36,16 @@ deb <- Sys.time()
 # We load a function to run PSC simulations and return the 6 summary 
 # statistics used in this study:
 source("PSC_simulation_c.R") 
+nbcores <- 20
+gr <- 300
+nR <- 1000
+n_loc=40 # number of loci
+smplsize=100 # number of individuals to simulate
+Mu=5e-2
+#Expansion
+log10thetaOBS <- - 1.5
+log10thetaancOBS <- 1
+log10tauOBS <- -0.2
 #IBDSimExec<-"./ibdsimV2.0-win-i386.exe" #IBDsim executable if ran on Windows
 
 # If the user wants to run this script on a Linux platform, the IBDsim sources
@@ -65,50 +75,57 @@ if(get_os()=="linux") {
     IBDSimExec<-"../IBDSimMac"
     } else {IBDSimExec<-"../ibdsimv2.0_Win7.exe"}
 
+log10thetaOBS
+log10thetaancOBS
+log10tauOBS
 
-gr <- 300
-parsp <- init_grid(lower=c(log10theta=-2.5,log10thetaanc=-1.0,log10tau=-1),
-                   upper=c(log10theta=1.5,log10thetaanc=3.5,log10tau=1),
+gr_theta <- c(-3, 2)
+gr_thetaanc <- c(-1, 3)
+gr_tau <- c(-2,2)
+parsp <- init_grid(lower=c(log10theta=gr_theta[1],log10thetaanc=gr_thetaanc[1],log10tau=gr_tau[1]),
+                   upper=c(log10theta=gr_theta[2],log10thetaanc=gr_thetaanc[2],log10tau=gr_tau[2]),
                    nUnique=gr)
+
 
 
 #10^parsp[,1]/mu
 #10^parsp[,2]/mu
 #10^parsp[,3]*10^parsp[,1]/mu
 
-message("SIM: Nact(min/max)=",paste(min(10^parsp[,1]/mu),max(10^parsp[,1]/mu)," "),
-        "\nT(min/max)=",paste(min(10^parsp[,3]*10^parsp[,1]/mu),max(10^parsp[,3]*10^parsp[,1]/mu)," "),
-        "\nNanc(min/max)=",paste(min(10^parsp[,2]/mu),max(10^parsp[,2]/mu)," "))
+message("SIM: Nact(min/max)=",paste(min(10^parsp[,1]/Mu),max(10^parsp[,1]/Mu)," "),
+        "\nT(min/max)=",paste(min(10^parsp[,3]*10^parsp[,1]/Mu),max(10^parsp[,3]*10^parsp[,1]/Mu)," "),
+        "\nNanc(min/max)=",paste(min(10^parsp[,2]/Mu),max(10^parsp[,2]/Mu)," "))
 
 
 
 # We compute a pseudo-observed set of summary statistics with
 # with arbitrary parameters values, e.g. theta = 3, a = 3, tau = -1
-#Expansion
-log10thetaOBS <- -0.398
-log10thetaancOBS <- 1.30
-log10tauOBS <- 0.097
+
 
 
 #Grille
-hist(parsp$log10theta, breaks = 20)
-abline(v = log10thetaOBS, col="red")
-hist(parsp$log10thetaanc, breaks=20)
-abline(v = log10thetaancOBS, col="red")
-hist(parsp$log10tau, breaks=20)
-abline(v = log10tauOBS, col="red")
+op <- par(no.readonly = TRUE) # the whole list of settable "par" 's to be reset afterwards
+par(mfrow=c(2, 2)) # les modifs de "par" pour les graphiques ci dessous
+ hist(parsp$log10theta, breaks = 20)
+ abline(v = log10thetaOBS, col="red")
+ hist(parsp$log10thetaanc, breaks=20)
+ abline(v = log10thetaancOBS, col="red")
+ hist(parsp$log10tau, breaks=20)
+ abline(v = log10tauOBS, col="red") 
+par(mfrow=c(1, 1))
+par(op) #et a la fin on remet "par" comme avant
 
 
-sobs <- IBDSim_wrapper(log10theta=log10thetaOBS,log10thetaanc =log10thetaancOBS,log10tau=log10tauOBS,execName=IBDSimExec)
+sobs <- IBDSim_wrapper(log10theta=log10thetaOBS,log10thetaanc =log10thetaancOBS,log10tau=log10tauOBS, mu = Mu, sampleSize = smplsize, nloc=n_loc,execName=IBDSimExec)
 sobs
 
-message("OBS: Nact=",10^log10thetaOBS/mu,
-        "; T=",10^log10tauOBS*10^log10thetaOBS/mu,
-        "; Nanc=",10^log10thetaancOBS/mu)
+message("OBS: Nact=",10^log10thetaOBS/Mu,
+        "; T=",10^log10tauOBS*10^log10thetaOBS/Mu,
+        "; Nanc=",10^log10thetaancOBS/Mu)
 
-nR <- 1000
+
 et <- proc.time()
-simuls <- add_simulation(NULL,Simulate="IBDSim_wrapper",par.grid=parsp, nRealizations = nR, nb_cores = NULL)# nRealizations = 1
+simuls <- add_simulation(NULL,Simulate="IBDSim_wrapper",par.grid=parsp, nRealizations = nR, nb_cores = nbcores)# nRealizations = 1
 time <- proc.time()-et
 time
 
@@ -126,7 +143,7 @@ corrSobs <- project(sobs,projectors=list("THETA"=thetaproj,"THETAANC"=thetaancpr
 
 #load("PSC_simulations.rda") # loads the projected summary statistics
 # We then infer the summary-likelihood surface:
-densv <- infer_logLs(corrSimuls,stat.obs=corrSobs)
+densv <- infer_logLs(corrSimuls,stat.obs=corrSobs, nb_cores=nbcores)
 slik <- infer_surface(densv)
 slik <- MSL(slik)
 plot(slik, filled=TRUE) # seems to be valid ony for analyses with 2 parameters
@@ -134,36 +151,100 @@ plot(slik, filled=TRUE) # seems to be valid ony for analyses with 2 parameters
 
 # We iterate to improve parameter estimation:
 nIterations <- 3
-for(i in 1:nIterations) {
-  slik <- refine(slik)
+ntimes_iter <- 1
+nIterations_total <- ntimes_iter*nIterations
+for (j in 1:ntimes_iter) 
+{
+  for(i in 1:nIterations) 
+    {
+  name_slik <- paste("slik", i, sep="")
+  assign(name_slik, refine(slik, nb_cores=7))
+    }
 }
-#Error in .calc_all_slices(object, fittedPars, color.palette) : 
-# objet 'plot.axes' introuvable
+
+# Sauvegarder slik tous les 3 refine
 
 # The slik object contains the ML estimates and 95% CIs:
 summary(slik)
 
 # We can also get the x% CIs (here, 90% CIs):
-confint(slik,"log10theta",level=0.9)
-confint(slik,"log10thetaanc",level=0.9)
-confint(slik,"log10tau",level=0.9)
+confint_theta <- confint(slik,"log10theta",level=0.9)
+confint_thetaanc <- confint(slik,"log10thetaanc",level=0.9)
+confint_tau <- confint(slik,"log10tau",level=0.9)
 
 
+########################################################"
+
+# Calcul durée du script
+fin <- Sys.time()
+duree <- fin - deb
+
+# Type de simulation constraction, expansion, constant)
 name  <- NULL
 if (log10thetaancOBS>log10thetaOBS)
 {name <- "contr"
+}else if (log10thetaancOBS<log10thetaOBS){
+  name <- "exp"
 }else{
-  name <- "exp"}
+  name <- "cst"
+}
+
 
 setwd("/work/cvernier/")
-save.image(paste ("/work/cvernier/", 
-                  name, "th",log10thetaOBS, "thanc", log10thetaancOBS, "tau", log10tauOBS, 
-                  "g", gr,"nR", nR, "nloc", nloc, "smplsize", samplesize, "mu", mu, deb,"sim.RData", sep="_"))
+exist <- exists("slik")
 
-png(file = paste ("/work/cvernier/", deb, "plot1D.png", sep="_"))
-plot1Dprof(slik)
+# Classement de la table selon si slik a fonctionné ou non
+if (exist == "TRUE")
+{
+  chemin <- "/work/cvernier/ok/"
+}else{
+  chemin <- "/work/cvernier/bug/"
+}
+
+# Sauvegarde données Rdata
+save.image(paste (chemin, 
+                  name, "log10theta =",log10thetaOBS, "log10thetaanc =", log10thetaancOBS, "log10tau =", log10tauOBS, 
+                  "taille grille =", gr,"nRealizations =", nR, "nloc =", n_loc, "smplsize =", smplsize, "mu =", Mu, 
+                  deb,".RData", sep=" "))
+
+
+# Sauvegarde des paramètres dans un fichier txt
+dat <- capture.output(slik)
+sobs_dat <- capture.output(sobs)
+write(paste(name, "\nlog10theta =",log10thetaOBS, " Bornes theta = [", gr_theta[1], ",", gr_theta[2], "]", 
+            "\nlog10thetaanc =", log10thetaancOBS, " Bornes thetaanc =[", gr_thetaanc[1], ",", gr_thetaanc[2], "]", 
+            "\nlog10tau =", log10tauOBS, " Bornes tau =[", gr_tau[1], ",", gr_tau[2], "]",
+            "\n\nOBS: Nact=",10^log10thetaOBS/Mu,"; T=",10^log10tauOBS*10^log10thetaOBS/Mu,"; Nanc=",10^log10thetaancOBS/Mu,
+            "\n\n", sobs_dat[1], "\n", sobs_dat[2],
+            "\n\nSIM: Nact(min/max)=",paste(min(10^parsp[,1]/Mu),max(10^parsp[,1]/Mu)," "),
+            "\nT(min/max)=",paste(min(10^parsp[,3]*10^parsp[,1]/Mu),max(10^parsp[,3]*10^parsp[,1]/Mu)," "),
+            "\nNanc(min/max)=",paste(min(10^parsp[,2]/Mu),max(10^parsp[,2]/Mu)," "),
+            "\n\ntaille grille =", gr, "\nnRealizations =", nR, "\nnloc =", n_loc, "\nsmplsize =", smplsize, "\nmu =", Mu,  
+            "\nNb refine =", nIterations_total, 
+            "\n\nConfint theta = [", confint_theta$lowerpar[1], ",", confint_theta$upperpar[1], "]",
+            "\nConfint theta anc = [", confint_thetaanc$lowerpar[2], ",", confint_thetaanc$upperpar[2], "]",
+            "\nConfint tau = [", confint_tau$lowerpar[3], ",", confint_tau$upperpar[3], "]",
+            "\n\n", dat[1], "\n", dat[2], "\n", dat[3], "\n", dat[4],"\n", dat[5], "\n", dat[6], "\n", dat[7], "\n", dat[8],
+            "\n","\n", capture.output(duree), "\n", deb, sep=" "), 
+              file=paste(deb,".txt"))
+  
+# Sauvegarde des graphiques (en cours)
+pdf(file = paste (chemin, deb, "plot.pdf", sep="_"))
+ op <- par(no.readonly = TRUE) # the whole list of settable "par" 's to be reset afterwards
+ par(mfrow=c(2, 2)) # les modifs de "par" pour les graphiques ci dessous
+  hist(parsp$log10theta, breaks = 20)
+  abline(v = log10thetaOBS, col="red")
+  hist(parsp$log10thetaanc, breaks=20)
+  abline(v = log10thetaancOBS, col="red")
+  hist(parsp$log10tau, breaks=20)
+  abline(v = log10tauOBS, col="red") 
+ par(mfrow=c(1, 1))
+ par(op) #et a la fin on remet "par" comme avant
+  plot(slik)
+  plot1Dprof(slik)
+  plot(thetaproj)
+  plot(thetaancproj)
+  plot(tauproj)
 dev.off()
 
-png(file = paste ("/work/cvernier/", deb, "plot2D.png", sep="_"))
-plot2Dprof(slik)
-dev.off()
+
