3601daaa59e630f66ba9a328be3bdb5996a909ba
Raphael Leblois <raphael.leblois@wanadoo.fr>
2018-07-10 18:50:50 +0200

    update Raph's scripts and one dump

--- IBD_inference_Raph.R
+++ IBD_inference_Raph.R
@@ -2,7 +2,7 @@ rm(list = ls())
 if (interactive()) {options(error=recover)} else {
   options(echo = FALSE)
   options(error = quote(dump.frames(paste("./bug/dump",deb, "__grille=",gr, "__nloc=",n_loc, sep=""), TRUE)))
-  options(warn=2)
+  options(warn=1)
 }
 get_os <- function(){
   sysinf <- Sys.info()
@@ -28,7 +28,7 @@ library(doSNOW)
 #setwd(dir = "/work/cvernier/Infusion")
 #setwd(dir="/home/vernierc/Documents/GitCamille/Version locale/")
 setwd(dir="/Users/raph/Downloads/++Ajeter/Camille/")
-source("IBD_simulation_FR.R") 
+source("IBD_simulation_Raph.R") 
 # if(get_os()=="linux") {
 #   IBDSimExec<-"../IBDSim"
 # }  else if(get_os()=="osx") {
@@ -37,48 +37,51 @@ source("IBD_simulation_FR.R")
 IBDSimExec<-"/Users/raph/Downloads/++Ajeter/Camille/IBDSim"
 
 ########################################################
-nbcores <- 1
-gr <- 300
+nbcores <- 5
+gr <- 50
 nR <- 1
 
 # gshape <- 0.25
 # em_rate <- 0.45
-gshape <- 0.575
-em_rate <- 0.25
-habitatSizeObs <- 70
+g_shape_obs <- 0.575
+em_rate_obs <- 0.25
+hab_size_obs <- 70
 
-gr_gshape <- c(0, 1)
-gr_emigrate <- c(0, 1)
-habitatSize_bounds <-c(16,400)
+g_gshape_bounds <- c(0, 1)
+em_rate_bounds <- c(0, 1)
+hab_size_bounds <-c(16,400)
 
 deb <- Sys.time()
 
-parsp <- init_grid(lower=c(g_shape=gr_gshape[1],m=gr_emigrate[1],habitatSize=habitatSize_bounds[1]),
-                   upper=c(g_shape=gr_gshape[2],m=gr_emigrate[2],habitatSize=habitatSize_bounds[2]),
+parsp <- init_grid(lower=c(g_shape=g_gshape_bounds[1],em_rate=em_rate_bounds[1],hab_size=hab_size_bounds[1]),
+                   upper=c(g_shape=g_gshape_bounds[2],em_rate=em_rate_bounds[2],hab_size=hab_size_bounds[2]),
                    nUnique=gr)
 parsp2 <- unique(parsp)
 
-sobs <- IBDSim_wrapper_IBD(habitatSize = habitatSizeObs, g_shape = gshape, m = em_rate, execName=IBDSimExec)
+sobs <- IBDSim_wrapper_IBD(hab_size = hab_size_obs, g_shape = g_shape_obs, em_rate = em_rate_obs, execName=IBDSimExec)
 sobs
 
 ###############
 Infusion.options(nb_cores = c(param=nbcores))
 rm(simtable,slik_j,dens)
-simtable <- add_reftable(Simulate="IBDSim_wrapper_IBD",par.grid=parsp2, nb_cores = c(param=nbcores))
+simtable <- add_reftable(Simulate="IBDSim_wrapper_IBD",execName=IBDSimExec, par.grid=parsp2, nb_cores = c(param=nbcores))
 
 allstats <- c("Hobs_moy","varHobs","Hexp_moy","varHexp","fis_moy","fis","nb_allele_moyTotalSample","var_nballele","ar_slope","ar_intercept","er_slope","er_intercept")
 
 gproj <- project("g_shape",stats=allstats,data=simtable,method="neuralNet")
-mproj <- project("emmig_rate",stats=allstats,data=simtable,method="neuralNet")
-habsizeproj <- project("habitat_size",stats=allstats,data=simtable,method="neuralNet")
+mproj <- project("em_rate",stats=allstats,data=simtable,method="neuralNet")
+habsizeproj <- project("hab_size",stats=allstats,data=simtable,method="neuralNet")
 
 # We apply projections on simulated statistics:
-corrSimuls <- project(simtable,projectors=list("g"=gproj,"m"=mproj,"habsize"=habsizeproj))
-corrSobs <- project(sobs,projectors=list("g"=gproj,"m"=mproj,"habsize"=habsizeproj))
+corrSimuls <- project(simtable,projectors=list("g_shape_proj"=gproj,"em_rate_proj"=mproj,"hab_size_proj"=habsizeproj))
+corrSobs <- project(sobs,projectors=list("g_shape_proj"=gproj,"em_rate_proj"=mproj,"hab_size_proj"=habsizeproj))
 
-dens <- infer_SLik_joint(simtable,stat.obs=drop(sobs))
+#dens <- infer_SLik_joint(simtable,stat.obs=drop(sobs))
+dens <- infer_SLik_joint(corrSimuls,stat.obs=drop(corrSobs))
 
 slik_j <-MSL(dens)
+# The slik object contains the ML estimates and 95% CIs:
+summary(slik)
 plot(slik_j)
 slik_j <- refine(slik_j,maxit=6,nb_cores = c(param=nbcores))
 plot(slik_j)
--- IBD_simulation_Raph.R
+++ IBD_simulation_Raph.R
@@ -8,10 +8,12 @@ IBDSim_wrapper_IBD <-function(
   nloc=20, # number of loci
   mu=5e-4, # mutation rate
   g_shape, # geometric shape
-  m, # total emigration rate
-  habitatSize=NULL,
-  dist_max=20,
-  execName="/Users/raph/Downloads/++Ajeter/Camille/IBDSim") { # executable name
+  em_rate, # total emigration rate
+  hab_size=NULL,
+  dist_max=20
+  ,execName=NULL
+  #,execName="/Users/raph/Downloads/++Ajeter/Camille/IBDSim" # executable name
+  ) { 
   
   curDir<-getwd()
   dir1 <- tempfile(pattern = "sim", tmpdir =curDir )
@@ -20,9 +22,9 @@ IBDSim_wrapper_IBD <-function(
   #message("Files in the currect directory: ", paste(list.files(path = ".", all.files = TRUE,recursive = TRUE, include.dirs = TRUE, no.. = TRUE)," "))
   
   Seed <- 1234567 + sample(1:10000, 1)
-  if ( ! is.null(habitatSize) ) {
-    habitatSize <- floor(habitatSize);
-    lattice <- c(habitatSize,habitatSize);
+  if ( ! is.null(hab_size) ) {
+    hab_size <- floor(hab_size);
+    lattice <- c(hab_size,hab_size);
     min_sample=c( floor( floor(lattice[1]/2) - floor(samp[1]/2) ) ,floor( floor(lattice[2]/2) - floor(samp[2]/2) ) );
   }
   
@@ -57,7 +59,7 @@ write.table(paste("%%%%% SIMULATION PARAMETERS %%%%%%%%%%%%
                     %% DISPERSAL
                     Dispersal_Distribution=g
                     Geometric_Shape=",g_shape,"
-                    Total_Emigration_Rate=",m,"
+                    Total_Emigration_Rate=",em_rate,"
                     MinDistReg=0.000001
                     Dist_max=",dist_max,"
                     
@@ -108,158 +110,7 @@ write.table(paste("%%%%% SIMULATION PARAMETERS %%%%%%%%%%%%
                         %% DISPERSAL
                         Dispersal_Distribution=g
                         Geometric_Shape=",g_shape,"
-                        Total_Emigration_Rate=",m,"
-                        MinDistReg=0.000001
-                        Dist_max=",dist_max,"
-                        
-                        DiagnosticTables=Hexp,Fis,Iterative_Statistics,arRegression,erRegression,Iterative_Identity_Probability
-                        noSS=True",sep=""),
-                  file="IbdSettings.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
-      system(execName, ignore.stdout = TRUE)
-    } else {
-      dat<-sumstats[,(dim(sumstats)[2]-7-samp[1]):dim(sumstats)[2]] #on prend les stats qui nous intéressent
-      #colnames(dat) <- sumstats_name[,(dim(sumstats)[2]-8):dim(sumstats)[2]]
-      qnames<-NULL
-      for (i in 1:samp[1]) 
-      {
-        name <- paste("Qr(", i-1, ")", sep="")
-        qnames <- c(qnames,name)
-      }
-      
-      colnames(dat)<-c("Hobs_moy","Hexp_moy","fis_moy","nb_allele_moyTotalSample",qnames
-                       , "ar_slope","ar_intercept","er_slope","er_intercept")
-      
-      hobs<-read.table("Iterative_Statistics_postdisp_PerLocus.txt",sep="",skip=1)[,1:nloc]
-      hexp<-read.table("Iterative_Statistics_postdisp_PerLocus.txt",sep="",skip=1)[,(nloc+1):(2*nloc)]  
-      dat$varHobs<-apply(hobs,1,var)
-      dat$varHexp<-apply(hexp,1,var)
-      hobs_moy <- as.numeric(dat[,1])
-      hexp_moy <- as.numeric(dat[,2])
-      dat$fis <- 1-(hobs_moy/hexp_moy)
-      nballele <- read.table("Iterative_Statistics_postdisp_PerLocus.txt",sep="",skip=1)[,(3*nloc+1):(4*nloc)]  
-      dat$var_nballele <- apply(nballele,1,var)
-      #dat$Dsigma2 <- D*((m*(1+g_shape))/(1-g_shape)^2)
-      data2 <- dat[,c("Hobs_moy","varHobs","Hexp_moy","varHexp","fis_moy","fis","nb_allele_moyTotalSample","var_nballele","ar_slope","ar_intercept","er_slope","er_intercept")]  
-      
-      setwd("../")
-      if(tries<2) unlink(dir1, recursive =TRUE) 
-      
-      if(dim(data2)[1]>1) {data2<-t(as.matrix(data2))} else data2<-unlist(data2) # required to be read by Infusion
-      
-      break
-      
-    }
-  }
-  
-  return(data2)
-}
-
-###################################################################################################
-
-IBDSim_wrapper_IBD_Git <-function(
-  lattice=c(100,100),
-  samp=c(15,15),
-  min_sample=c(45,45),
-  D=1,
-  nsim=1,
-  nloc=20, # number of loci
-  mu=5e-4, # mutation rate
-  g_shape, # geometric shape
-  m, # total emigration rate
-  dist_max=20,
-  execName="/Users/raph/Downloads/++Ajeter/Camille/IBDSimGit"){ # executable name
-  
-  curDir<-getwd()
-  dir1 <- tempfile(pattern = "sim", tmpdir =curDir )
-  dir.create(dir1)
-  setwd(dir = dir1)
-  #message("Files in the currect directory: ", paste(list.files(path = ".", all.files = TRUE,recursive = TRUE, include.dirs = TRUE, no.. = TRUE)," "))
-  
-  Seed <- 1234567 + sample(1:10000, 1)
-  
-  # we write the input file for IBDsim:
-  
-  write.table(paste("%%%%% SIMULATION PARAMETERS %%%%%%%%%%%%
-                    Run_Number=",nsim,"
-                    Migraine_Settings=F
-                    Ploidy=Diploid
-                    Random_Seeds=",Seed,"
-                    
-                    %%%%% MARKERS PARAMETER S%%%%%%%%%%%%%%%
-                    Locus_Number=",nloc,"
-                    Mutation_Rate=",format(mu, scientific = FALSE),"
-                    Mutation_Model=GSM
-                    Allelic_Upper_Bound=200
-                    Min_Allele_Number=2
-                    
-                    %%%%%%%% DEMOGRAPHIC OPTIONS %%%%%%%%%%%%%
-                    %% LATTICE
-                    Lattice_SizeX=",lattice[1],"
-                    Lattice_SizeY=",lattice[2],"
-                    Ind_Per_Pop=",D,"
-                    
-                    %% SAMPLE
-                    Sample_SizeX=",samp[1],"
-                    Sample_SizeY=",samp[2],"
-                    Min_Sample_CoordinateX=",min_sample[1],"
-                    Min_Sample_CoordinateY=",min_sample[2],"
-                    Ind_Per_Pop_Sampled=1
-                    
-                    %% DISPERSAL
-                    Dispersal_Distribution=g
-                    Geometric_Shape=",g_shape,"
-                    Total_Emigration_Rate=",m,"
-                    MinDistReg=0.000001
-                    Dist_max=",dist_max,"
-                    
-                    DiagnosticTables=Hexp,Fis,Iterative_Statistics,arRegression,erRegression,Iterative_Identity_Probability
-                    noSS=T",sep=""),
-              file="IbdSettings.txt",quote=FALSE,row.names=FALSE,col.names=FALSE)
-  
-  system(execName, ignore.stdout = TRUE)
-  #sumstats_name <- as.matrix(read.table("Iterative_Statistics_postdisp_PerLocus.txt",sep="", nrows=1))
-  #sumstats_name2 <- as.vector(sumstats_name[1,])
-  
-  tries <- 1
-  while (tries<4) {
-    sumstats<-try(read.table("Iterative_Statistics_postdisp_PerLocus.txt",sep="",skip=1))
-    if(inherits(sumstats,"try-error")) {
-      message(paste("Reading simulated summary statistics failed after ", tries, " tries."))
-      tries <- tries + 1;
-      Seed <- ( 1234567 + sample(1:10000, 1) );
-      
-      # we write the input file for IBDsim:
-      
-      write.table(paste("%%%%% SIMULATION PARAMETERS %%%%%%%%%%%%
-                        Run_Number=",nsim,"
-                        Migraine_Settings=F
-                        Ploidy=Diploid
-                        Random_Seeds=",Seed,"
-                        
-                        %%%%% MARKERS PARAMETER S%%%%%%%%%%%%%%%
-                        Locus_Number=",nloc,"
-                        Mutation_Rate=",format(mu, scientific = FALSE),"
-                        Mutation_Model=GSM
-                        Allelic_Upper_Bound=200
-                        Min_Allele_Number=2
-                        
-                        %%%%%%%% DEMOGRAPHIC OPTIONS %%%%%%%%%%%%%
-                        %% LATTICE
-                        Lattice_SizeX=",lattice[1],"
-                        Lattice_SizeY=",lattice[2],"
-                        Ind_Per_Pop=",D,"
-                        
-                        %% SAMPLE
-                        Sample_SizeX=",samp[1],"
-                        Sample_SizeY=",samp[2],"
-                        Min_Sample_CoordinateX=",min_sample[1],"
-                        Min_Sample_CoordinateY=",min_sample[2],"
-                        Ind_Per_Pop_Sampled=1
-                        
-                        %% DISPERSAL
-                        Dispersal_Distribution=g
-                        Geometric_Shape=",g_shape,"
-                        Total_Emigration_Rate=",m,"
+                        Total_Emigration_Rate=",em_rate,"
                         MinDistReg=0.000001
                         Dist_max=",dist_max,"
                         
